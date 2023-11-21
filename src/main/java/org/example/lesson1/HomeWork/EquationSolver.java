788a3a2c3ac329d3a1f58d8bed631aa05dfd703f
AntonKaliyta <kaluta99@gmail.com>
2023-11-21 19:08:40 +0700

    HW1 complited

---
+++ src/main/java/org/example/lesson1/HomeWork/EquationSolver.java
@@ -0,0 +1,24 @@
+package org.example.lesson1.HomeWork;
+
+public class EquationSolver {
+    public static void main(String[] args) {
+        for (int q = 0; q <= 9; q++) {
+            for (int w = 0; w <= 9; w++) {
+                String equation = q + "?" + " + " + "?" + w + " = " + (q + w);
+                if (isValidEquation(equation)) {
+                    System.out.println("Решение: " + equation);
+                    return;
+                }
+            }
+        }
+        System.out.println("Решение не найдено");
+    }
+
+    public static boolean isValidEquation(String equation) {
+        String[] parts = equation.split("[+=]");
+        int q = Integer.parseInt(parts[0]);
+        int w = Integer.parseInt(parts[1].substring(0, 1));
+        int e = Integer.parseInt(parts[1].substring(1));
+        return (q + w == e);
+    }
+}
